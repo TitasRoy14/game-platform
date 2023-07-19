a35b2d8beeee391ef95d8cf23884532478083ae3
@TitasRoy14 <roytitas800@gmail.com>
2023-07-19 21:00:05 +0530

    Display generes

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,7 +16,7 @@ const GameGrid = () => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
-        spacing="9"
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((Skeleton) => (
