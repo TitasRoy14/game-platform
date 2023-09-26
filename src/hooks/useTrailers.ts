b977e60daa19686688518beeadc6c2483e265e58
TitasRoy14 <roytitas800@gmail.com>
2023-09-26 16:25:24 +0530

    this is only edited

--- src/hooks/useTrailers.ts
+++ src/hooks/useTrailers.ts
@@ -1,12 +1,12 @@
-import { useQuery } from "@tanstack/react-query";
-import Trailer from "../entities/Trailer";
-import APIClient from "../services/consumer-client";
+import { useQuery } from '@tanstack/react-query';
+import Trailer from '../entities/Trailer';
+import APIClient from '../services/consumer-client';
 
 const useTrailers = (gameId: number) => {
   const apiClient = new APIClient<Trailer>(`/games/${gameId}/movies`);
 
   return useQuery({
-    queryKey: ["trailers", gameId],
+    queryKey: ['trailers', gameId],
     queryFn: apiClient.getAll,
   });
 };
