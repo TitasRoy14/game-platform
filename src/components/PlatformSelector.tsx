5684570e21db505bca7a0e3d4336349ea27d6b14
TitasRoy14 <roytitas800@gmail.com>
2024-06-05 14:19:11 +0530

    Fixed platforms issue

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,13 +1,15 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
-import { BsChevronDown } from "react-icons/bs";
-import usePlatform from "../hooks/usePlatform";
-import usePlatforms from "../hooks/usePlatforms";
-import useGameQueryStore from "../store";
+import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
+import { BsChevronDown } from 'react-icons/bs';
+import usePlatform from '../hooks/usePlatform';
+import usePlatforms from '../hooks/usePlatforms';
+import useGameQueryStore from '../store';
 
 const PlatformSelector = () => {
   const selectedPlatformId = useGameQueryStore((s) => s.gameQuery.platformId);
   const selectedPlatform = usePlatform(selectedPlatformId);
   const { data, error } = usePlatforms();
+
+  // console.log(data);
 
   const setSelectedPlatformId = useGameQueryStore((s) => s.setPlatformId);
 
@@ -15,11 +17,14 @@ const PlatformSelector = () => {
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform?.name || "Platforms"}
+        {selectedPlatform?.name || 'Platforms'}
       </MenuButton>
       <MenuList>
         {data?.results.map((platform) => (
-          <MenuItem key={platform.id} onClick={() => platform.id}>
+          <MenuItem
+            key={platform.id}
+            onClick={() => setSelectedPlatformId(platform.id)}
+          >
             {platform.name}
           </MenuItem>
         ))}
